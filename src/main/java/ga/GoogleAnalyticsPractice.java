b4d96f627c104d7c2d1cb9b10542ef29dd8a778d
Ailiartsua <ailiartsual2@gmail.com>
2023-02-14 10:56:12 +0900

    GA 필기

---
+++ src/main/java/ga/GoogleAnalyticsPractice.java
@@ -0,0 +1,51 @@
+package ga;
+
+public class GoogleAnalyticsPractice {
+    public static void main(String[] args) {
+        /**
+         * GoogleAnalytics Data API
+         * GA4 보고서 데이터에 프로그래밍적인 방식으로 액세스 가능,,
+         *  - 클래스 타입
+         *      - CohortSpec : 동질 집단 보고서의 동질 집단 사양
+         *      - DateRange
+         *      - Dimension
+         *      - DimensionHeader
+         *      - DimensionMetadata
+         *      - DimensionValue
+         *      - FilterExpression
+         *      - Metric
+         *      - MetricAggregation
+         *      - MetricHeader
+         *      - MetricMetadata
+         *      - Metric Type
+         *      - OrderBy
+         *      - Pivot
+         *      - PropertyQuota
+         *      - ResponseMetaData
+         *      - Row
+         *      - RunPivotReportResponse
+         *      - RunReportResponse
+         */
+
+
+        /**
+         * CohortSpec
+         * APIDocs url : https://developers.google.com/analytics/devguides/reporting/data/v1/rest/v1beta/CohortSpec?hl=ko
+         * 필드
+         *  - cohorts[] : 동질 집단으로 사용자를 그룹화 하기 위한 선택 기준을 정의
+         *  - cohortsRange : 동질 집단의 보고 기간을 정하는 오프셋
+         *  - cohortReportSettings : 동질 집단 보고서의 설정(선택사항)
+         */
+
+        /**
+         * CohortSpec
+         * APIDocs url : https://developers.google.com/analytics/devguides/reporting/data/v1/rest/v1beta/CohortSpec?hl=ko
+         * 필드
+         *  - cohorts[] : 동질 집단으로 사용자를 그룹화 하기 위한 선택 기준을 정의
+         *  - cohortsRange : 동질 집단의 보고 기간을 정하는 오프셋
+         *  - cohortReportSettings : 동질 집단 보고서의 설정(선택사항)
+         */
+
+
+    }
+}
