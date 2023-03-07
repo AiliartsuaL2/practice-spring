d16929605aca48a41b51cda6bb2c0b365f8d10d2
Juho <ailiartsual2@gmail.com>
2023-03-07 15:15:18 +0900

    Mybatis - MySQL 필기

---
+++ src/main/java/jdbc/Mybatis.java
@@ -0,0 +1,16 @@
+package jdbc;
+
+public class Mybatis {
+    /**
+     *  namespace는 mapper(인터페이스)위치를 작성
+     *  resultMap : 컬럼과 매핑할 map을 정의해둠,,
+     *  parameterType : 파라미터로 전달할 클래스 파일을 정의
+     *  id : Mapper 인터페이스의 메서드명과 동일하게 처리,
+     *
+     *
+     *  MySQL
+     *  DATE_FORMAT : 날짜 형식의 데이터를 String형태로 표현시킴,, 데이터 형식은 구글링 참고
+     *  NOW() : 현재 시간 표현,, TIMESTAMP 형식,, TIMESTAMP는 java에서 DATE 클래스로 매핑 할 수 있다.
+     *
+     */
+}
